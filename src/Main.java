60fd0079288237b637c4809000ecdfea94d69743
Максим <gmax7911@gmail.com>
2023-03-21 08:29:52 +0300

    2 задача готова

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Main {
+public abstract class Main {
     public static void main(String[] args) {
         Stream<Integer> stream = new ArrayList<>(Arrays.asList(1, 2, 4, 6, 8, 17, 33)).stream();
 
@@ -14,5 +16,15 @@ public class Main {
         );
 
         stream.close();
+
+        //Задание 2
+        System.out.println("Количество четных чисел " + countEvenNumber(stream.collect(Collectors.toList())));
+    }
+
+    public static int countEvenNumber(List<Integer> integers) {
+        return (int) integers.stream()
+                .filter(i -> i % 2 == 0)
+                .peek(System.out::println)
+                .count();
     }
 }
